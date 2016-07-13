46e81ae6b2041cf0777c47fdc65c5d92c51cdbc2
Altair Pinto <altairpet@gmail.com>
2016-07-13 16:38:06 -0300

    Inserção da API da Conductor
    Utilizada a API como uma Lib a partir do arquivo classes.jar e agora
    serão tratados os antigos dados estáticos de cartão/conta/cliente

--- CardTracker/app/src/main/java/br/com/cardtracker/Compra.java
+++ CardTracker/app/src/main/java/br/com/cardtracker/Compra.java
@@ -1,7 +1,8 @@
 package br.com.cardtracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class Compra extends AppCompatActivity {
 
